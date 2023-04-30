dbdb4debbcd022b06bd99deb3a4ddf8de0c5818f
Tj <tsevak86@gmail.com>
2023-04-30 15:55:30 +1200

    Add basic web server with Security group
    replace Amazon linux with Ubuntu  AMI
    Add basic web server to test connection
    Add security group with required port access
    Attach security group to ec2 instance

--- Ec2-Instance/main.tf
+++ Ec2-Instance/main.tf
@@ -1,12 +1,32 @@
 provider "aws" {
-    region = "ap-southeast-2"
+  region = "ap-southeast-2"
 }
 
 resource "aws_instance" "this" {
-    ami = "ami-0f6ad051716c81af1"         # AWS LINUX
-    instance_type = "t2.micro"
+  ami                    = "ami-03d0155c1ef44f68a" # Ubuntu Server 20.04 LTS
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-    tags = {
-      Name = "Example-terraform"
-    }
+  tags = {
+    Name = "Example-terraform"
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hi, Welcome to my webpage" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+  user_data_replace_on_change = true
+}
+
+resource "aws_security_group" "instance" {
+  name = "tf-this-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
