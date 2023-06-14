5404f353476f68e58fffa3f971c8c26d6973b4c5
Tj <tsevak86@gmail.com>
2023-06-14 12:23:15 +1200

    Add AWS_EKS config
    * create cluster
    * add managed worker nodes

---
+++ Receipes/EKS/main.tf
@@ -0,0 +1,64 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "dev-vpc"
+  cidr = "192.168.0.0/16"
+
+  azs             = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
+  private_subnets = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+  public_subnets  = ["192.168.101.0/24", "192.168.102.0/24", "192.168.103.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "dev-cluster"
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {}
+
+  eks_managed_node_groups = {
+    general = {
+      min_size     = 3
+      max_size     = 5
+      desired_size = 3
+
+      instance_types = ["t3.large"]
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
