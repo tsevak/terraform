75d48284910a7c1f3fa667fb8a88b409bc50756e
Tj <tsevak86@gmail.com>
2023-06-15 12:31:57 +1200

    Update worker node capacity

--- Receipes/EKS/main.tf
+++ Receipes/EKS/main.tf
@@ -49,9 +49,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     general = {
-      min_size     = 2
+      min_size     = 1
       max_size     = 3
-      desired_size = 2
+      desired_size = 1
 
       instance_types = ["t3.large"]
     }
